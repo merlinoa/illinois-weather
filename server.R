2a33a5a3d5804d89d3df3043206e2b39f97d9eb9
merlinoa <merlinoa88@gmail.com>
2015-07-25 12:28:05 -0400

    got colors working using hex

--- server.R
+++ server.R
@@ -3,20 +3,31 @@ library(rnoaa)
 library(shiny)
 library(threejs)
 
-# We had some rendering problems serving these images directly, it
-# seems to work more reliably if we send them as dataURIs.
 earth_dark   <- system.file("images/world.jpg",package="threejs")
 res <- storm_shp(year = 2013)
 storm_data <- as.data.frame(storm_shp_read(res)[, 1:10])
 storms <- unique(storm_data$Serial_Num)
-#colors <- sample(colors(), length(storms), replace = TRUE)
-colors <- paste(sample(0:255,size=length(storms),replace=TRUE),collapse=" ")
+colors <- c()
+for (i in seq_along(storms)) {  
+  colors[i] <- 
+    paste0(
+      c("#",
+        sample(
+          c("0", "1", "2", "3", "4", "5", "6", "7", "8" , "9",
+            "A", "B", "C", "D", "E", "F"
+          ),
+          size = 6 ,
+          replace=TRUE
+        )
+      ),
+      collapse = ""
+    )
+}
 storms <- data.frame(storms, colors)
+storms$colors <- as.character(storms$colors)
 names(storms) <- c("Serial_Num", "colors")
 storms_data <- left_join(storm_data, storms, by = "Serial_Num")
-#col <- list(
-#  earth_dark=list(img=earth_dark,bodycolor="#0011ff",emissive="#0011ff",lightcolor="#99ddff")
-#)
+
 
 shinyServer(function(input, output) {
   
@@ -25,10 +36,8 @@ shinyServer(function(input, output) {
             long = storms_data$coords.x1,
             color = storms_data$colors,
             img = earth_dark,
-            value = storms_data$wmo_wind / 20)
-            #bodycolor="#0011ff",
-            #emissive="#0011ff",
-            #lightcolor="#99ddff")
+            value = storms_data$wmo_wind / 40
+            )
   })
   
 })
